ef8858c3c635f33b32844b36915f546b5e72329f
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-10 23:32:11 +0100

    Add Input Lesson

---
+++ Variables.py
@@ -0,0 +1,14 @@
+#price = 10 # integer are whole numbers
+#rating = 4.9 # floats / floating point numbersare numbers with a DP
+#name = "david" # string
+#is_published = True #boolean values note the capital
+
+#print(price, rating)
+
+##Exercise
+full_name = ("John Smith")
+age = 20
+is_new =  True
+print(full_name)
+print(age)
+print(is_new)
