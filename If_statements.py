a60eaa2fa43dda400280f73c96b087913f61c079
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-14 20:49:46 +0100

    Add learing about if else and elif

---
+++ If_statements.py
@@ -0,0 +1,21 @@
+#is_hot = False
+#is_cold = False
+
+#if is_hot:
+#    print("It's a hot day")
+#    print("Drink plenty of water")
+#elif is_cold:
+#    print("It's a cold day")
+#    print("Wear warm clothes")
+#else:
+#    print("It's a lovely day")
+
+price = 1000000
+has_good_credit = True
+
+if has_good_credit:
+        down_payment = price * 0.1
+else:
+    down_payment = price * 0.2
+
+print(f"Downpayment: ${down_payment}")
