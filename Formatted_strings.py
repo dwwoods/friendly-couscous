3aa6994cfaa790b71e1600a941748051247f52df
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-14 12:44:10 +0100

    Add Formatted_strings.py lesson on how to format strings using f''

---
+++ Formatted_strings.py
@@ -0,0 +1,8 @@
+first = "John"
+last = "Smith"
+message = first + ' [' + last + '] is a coder ' #note the ' same
+msg = f'{first} [{last}] is a coder ' #how you format a string
+print(first)
+print(last)
+print(message)
+print(msg)
