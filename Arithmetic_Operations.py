1e5c0a3a39d5a44c4b31c58fb023ff6cf0f322af
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-14 20:28:11 +0100

    Add to Arithmetic_Operations.py floats, intergers, operators and precedence

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,21 @@
+#intergers = 1
+#floats = 1.x i.e. with a decimal
+
+print(10 + 3)
+print(10 / 3)
+print(10 // 3) # // rounds
+print(10 % 3) # returns the remainder
+print(10 ** 3)
+
+#x = 10
+#x += 3 # is the same as  x = x + 3
+#x -= 3
+#print(x)
+
+x1 = 10 + 3 * 2 # operator precedence
+print(x1)
+x = (2 + 3) * 10 - 3
+print(x)
+
+y = 10 + 3 * 2 **2
+print(y)
