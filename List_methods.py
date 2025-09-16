afe433d1ee7bcd5f7947573530e3af8e02087151
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-16 12:27:04 +0100

    Added .example methods to List_methods.py

--- List_methods.py
+++ List_methods.py
@@ -1,16 +1,27 @@
-# are v powerful - data science and ML
-#matrix is a list of lists.
-matrix = [
-    [1,2,3],
-    [4,5,6],
-    [7,8,9]
-]
-print(matrix[0][1])
-matrix[0][1] = 20
-print(matrix)
+#learning about .methods for list
+numbers = [5,2,1,5,4,7]
 
-for row in matrix:
-    for item in row:
-        print(item)
-for col in matrix:
-    print(col)
+numbers.append(20) #methods
+print(numbers)
+
+numbers.insert(0,10)
+print(numbers)
+
+numbers.remove(5)
+print(numbers)
+
+numbers.pop()
+print(numbers)
+print(numbers.count(5))
+print(numbers.index(4))
+print(50 in numbers) # boolean result
+print(10 in numbers)
+numbers.sort()
+print(numbers)
+numbers.reverse()
+print(numbers)
+numbers2 = numbers.copy()
+
+numbers.clear()
+print(numbers)
+print(numbers2)
