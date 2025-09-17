95428543857954f9de54f6b8b1becf5ba1e53669
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-17 10:52:04 +0100

    Learning about unpacking

---
+++ Unpacking.py
@@ -0,0 +1,18 @@
+coordinates = (1, 2, 3) #tuple
+print(coordinates)
+
+#This is the same
+x = coordinates[0]
+y = coordinates[1]
+z = coordinates[2]
+print(x,y,z)
+print(x)
+print(y)
+print(z)
+
+#as this
+x, y, z = coordinates
+print(x, y, z)
+print(x)
+print(y)
+print(z)
