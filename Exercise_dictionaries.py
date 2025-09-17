e7d13baa6a295a12668eb5695f74d5ffd00cd87b
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-17 11:24:58 +0100

    Exercise complete

---
+++ Exercise_dictionaries.py
@@ -0,0 +1,19 @@
+phone = input('Phone:')
+digtit_mapping = {
+     "1" : "one",
+    "2" : "two",
+    "3" : "three",
+    "4" : "four",
+    "5" : "five",
+    "6" : "six",
+    "7" : "seven",
+    "8" : "eight",
+    "9" : "nine",
+    "0" : "zero",
+}
+output = ""
+for ch in phone:
+    output += digtit_mapping.get(ch, "!") + " "
+print(output)
+
+
