addf2994e4b933745ce1b34b64d49386aa8b2b01
dwwoods <78119296+dwwoods@users.noreply.github.com>
2025-09-23 15:04:44 +0100

    Learn Constructors

---
+++ Constructors.py
@@ -0,0 +1,15 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def move(self): # you can define methods in the class
+        print("move")
+
+    def draw(self):
+        print("draw")
+
+
+point1 = Point(10, 20)
+print(point1.x, point1.y)
+point1.move()
+point1.draw()
